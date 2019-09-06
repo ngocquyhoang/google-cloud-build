b111424f998df9175071f85b8d7b37455e41d17e
ngocquyhoang <ngocquyhoang3112@gmail.com>
2019-09-06 13:51:27 +0700

    [Wordpress] Update Dockerfile and entrypoint

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-mkdir -p /root/.ssh
-echo "ServerAliveInterval 300" >> /root/.ssh/config 
-ssh-keyscan -H "$BUILD_HOST" >> /root/.ssh/known_hosts
-echo -e $BUILD_KEY >> /root/.ssh/id_rsa
-chmod 400 /root/.ssh/id_rsa
+mkdir -p ~/.ssh
+ssh-keyscan -H "$BUILD_HOST" >> ~/.ssh/known_hosts
+echo -e $BUILD_KEY >> ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
 
 if [ $? -eq 0 ]
 then
 	echo $'\n' "------ CONFIG SUCCESSFUL! ---------------------" $'\n'
-	echo $'\n' "-----------------------------------------------" $'\n'
-	cat /root/.ssh/id_rsa
-	echo $'\n' "-----------------------------------------------" $'\n'
 else
 	echo $'\n' "------ CONFIG FAILED! -------------------------" $'\n'
 	exit 1
 fi
+
+cd /app/deploy
 
 rsync --progress -avzh \
 	--exclude='.git/' \
 	--exclude='.git*' \
 	--exclude='cloudbuild.yml' \
 	--exclude='readme.md' \
-	-e "ssh -i /root/.ssh/id_rsa" \
-	--rsync-path="sudo rsync" . $BUILD_USER@$BUILD_HOST:$BUILD_PATH
+	-e "ssh -i ~/.ssh/id_rsa" \
+	--rsync-path="rsync" . $BUILD_USER@$BUILD_HOST:$BUILD_PATH
 
 if [ $? -eq 0 ]
 then
 	echo $'\n' "------ SYNC SUCCESSFUL! -----------------------" $'\n'
 	echo $'\n' "------ RELOADING PERMISSION -------------------" $'\n'
 
-	ssh -i /root/.ssh/id_rsa \
+	ssh -i ~/.ssh/id_rsa \
 		-t $BUILD_USER@$BUILD_HOST \
 		"sudo chown -R $BUILD_USER:$BUILD_USER $BUILD_PATH"
 
-	ssh -i /root/.ssh/id_rsa \
+	ssh -i ~/.ssh/id_rsa \
 		-t $BUILD_USER@$BUILD_HOST \
 		"sudo chmod 777 -R $BUILD_PATH"
 
